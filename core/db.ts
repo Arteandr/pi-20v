d492211fe828eeb707c18bd3b02bacf96c87cc0b
Arteandr <artedamatik@gmail.com>
2020-10-26 18:35:06 +0300

    configuring DB

---
+++ core/db.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+mongoose.Promise = Promise;
+
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.rwlam.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console,"Connection error"));
+
+export {db,mongoose};
--- server.ts
+++ server.ts
@@ -1,13 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
 
+dotenv.config();
 
 app.get('/users',():void => {});
 
 
 app.listen(8888,():void => {
-    console.log('test');
+    console.log('Server running');
     
 })
 
