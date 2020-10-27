0fc2bbe187af142a4c32d079069c24dfbd6a4005
Arteandr <artedamatik@gmail.com>
2020-10-27 15:41:53 +0300

    Add comments

--- server.ts
+++ server.ts
@@ -1,33 +1,42 @@
+/**---------------------------------IMPORTS---------------------------------*/
+                                /** Basic */    
 import express, { Application } from 'express';
+
+                    /** Dotenv import and configuring */
 import dotenv from 'dotenv';
-/** CONFIGURE DOTENV */
 dotenv.config();
 
-import { passport } from './core/passport';
-
-/** DATABASE(DB) */
+                              /** Database */
 import './core/db';
 
-/** CONTROLLERS */
-import { UserCtrl } from './controllers/UserController';
+                                /** Auth */
+import { passport } from './core/passport';
 
-/** VALIDATION */
+/**---------------------------------CONTROLLERS---------------------------------*/
+import { UserCtrl } from './controllers/UserController'; // User Controller
+
+/**---------------------------------VALIDATIONS---------------------------------*/
 import { registerValidations } from './validations/register';
 
-/** CREATE APP INSTANCE */
-const app: Application = express();
 
-/* MIDDLEWARES */
-app.use(express.json());
-app.use(passport.initialize());
+const app: Application = express(); // Create app instance
 
-/* ROUTES */
+/**---------------------------------MIDDLEWARES---------------------------------*/
+app.use(express.json());    // json body parser
+app.use(passport.initialize()); // auth initialize
+
+/*---------------------------------ROUTES---------------------------------*/
+                                /** Users */
 app.get('/users', UserCtrl.index);
 app.get('/users/me', passport.authenticate('jwt', {session: false}), UserCtrl.getUserInfo);
 app.get('/users/:id', UserCtrl.show);
+                                /** Auth */
 app.post('/auth/register', registerValidations, UserCtrl.create);
 app.post('/auth/login', passport.authenticate('local'), UserCtrl.afterLogin);
+                                /** Task */
 
+
+                                /** SERVER START */
 app.listen(8888, (): void => {
     console.log('Server running');
     
